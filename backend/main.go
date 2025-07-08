f3d27319305f70d1fcce3e0db7a7153b7213fd42
sakhisheikh <sakhi_mansoor@yahoo.com>
2025-07-08 21:46:24 +0200

    setup gin

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(); err != nil {
+		log.Fatalf("Failed to start the server")
+	}
+
+}
